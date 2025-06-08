47251cc20fe43978d4bde5d69741342c47862979
Phan Nguyen Khanh Minh <phannguyenkhanhminh@gmail.com>
2025-06-08 11:23:29 +0700

    Initial Commit

---
+++ MyTaksList/MyTaksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTaksListApp.swift
+//  MyTaksList
+//
+//  Created by Phan Nguyễn Khánh Minh on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTaksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
